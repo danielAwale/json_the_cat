4c0418c1533e96ded4061f12ea36c30cdad513c0
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-08-01 19:42:57 +0000

    have all the functionalities except for dynamic breed name

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,12 @@
 const request = require('request');
 
+request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, res, body) => {
+  if (error) {
+    console.log('cannot find details')
+  } else {
+    const data = JSON.parse(body);
+    console.log(typeof data)
+    const firstEntryData = data[0];
+    console.log(firstEntryData.description);
+  }
+});
