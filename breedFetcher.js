754ad8cb53b0d29583e3ffd6ec9b95b523ab10d7
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-08-01 19:55:04 +0000

    refactor code to make it easy to read and understand

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,19 @@
 const request = require('request');
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=sib`, (error, res, body) => {
-  if (error) {
-    console.log('cannot find details')
-  } else {
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, res, body) => {
+    if (error) {
+      callback('Request failed cannot find!')
+    }
     const data = JSON.parse(body);
-    console.log(typeof data)
+    // console.log(typeof data)
     const firstEntryData = data[0];
-    console.log(firstEntryData.description);
-  }
-});
+    if (firstEntryData) {
+      callback(null, firstEntryData.description);
+    } else {
+      callback("Failed to find requested data", null);
+    }
+  });
+}
+
+module.exports = { fetchBreedDescription };
